4aa0ec1c402ea026a0a8b3b1e1891ddf81d705dc
Jeremy Flowers <flowersje08@gmail.com>
2020-05-28 20:39:20 -0700

    initial setup finished, starting prompt codes

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,31 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+const writeFile = util.promisify(fs.writeFile);
+
+// copied from class example
+// const questions = [
+// ];
+
+inquirer
+    .prompt([
+        {
+            name: "projectTitle",
+            type: "input",
+            message: "What is the title of your project?"
+        },
+        {
+            name: "projectDescription",
+            type: "input",
+            message: "Describe your project."
+        },
+        {
+            name: "tableOfContents",
+            type: "input",
+            message:
+        }
+    ])
 
 function writeToFile(fileName, data) {
 }
