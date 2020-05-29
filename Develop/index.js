ccfebf4c501a4d55ec2a4160df9d1648b3396a40
Jeremy Flowers <flowersje08@gmail.com>
2020-05-28 23:33:13 -0700

    finished prompts, working on logging the resulting object

--- Develop/index.js
+++ Develop/index.js
@@ -13,19 +13,57 @@ inquirer
         {
             name: "projectTitle",
             type: "input",
-            message: "What is the title of your project?"
+            message: "What is the thing called?"
         },
         {
             name: "projectDescription",
             type: "input",
-            message: "Describe your project."
+            message: "Describe the thing:"
         },
         {
             name: "tableOfContents",
             type: "input",
             message: "List your table of contents"
+        },
+        {
+            name: "installation",
+            type: "input",
+            message: "How do you install the thing?"
+        },
+        {
+            name: "usage",
+            type: "input",
+            message: "How do you use the thing?"
+        },
+        {
+            name: "license",
+            type: "input",
+            message: "What license is the thing under?"
+        },
+        {
+            name: "contributors",
+            type: "input",
+            message: "Who made the thing?"
+        },
+        {
+            name: "tests",
+            type: "input",
+            message: "How do you test the thing?"
+        },
+        {
+            name: "questions",
+            type: "input",
+            message: "What kind of feedback do you need about the thing?"
+        },
+        {
+            name: "userGithub",
+            type: "input",
+            message: "What's your github account handle?"
         }
     ])
+    .then(function (user) {
+        console.log(user);
+    })
 
 function writeToFile(fileName, data) {
 }
